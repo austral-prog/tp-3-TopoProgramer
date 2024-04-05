ca6de0674bd3cb27e264da00af68fafe22397ee5
Bruno <116372845+TopoProgramer@users.noreply.github.com>
2024-04-05 12:40:22 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,6 @@
 def slice_advanced():
+    Palabra = input("Ingrese cualquier palabra/frase:")
+    print(Palabra[4::2])
     # Código a implementar utilizando input.
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
